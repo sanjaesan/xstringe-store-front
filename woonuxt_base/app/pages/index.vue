31d44e0fc0dd2a3d461e87df9c0d28811664294d
sanjaesan <sanusijubril.sj@gmail.com>
2025-02-25 07:50:44 +0100

    modifications

--- woonuxt_base/app/pages/index.vue
+++ woonuxt_base/app/pages/index.vue
@@ -23,12 +23,14 @@ useSeoMeta({
     <HeroBanner />
 
     <div class="container flex flex-wrap items-center justify-center my-16 text-center gap-x-8 gap-y-4 brand lg:justify-between">
-      <img src="/images/logoipsum-211.svg" alt="Brand 1" width="132" height="35" />
-      <img src="/images/logoipsum-221.svg" alt="Brand 2" width="119" height="30" />
-      <img src="/images/logoipsum-225.svg" alt="Brand 3" width="49" height="48" />
-      <img src="/images/logoipsum-280.svg" alt="Brand 4" width="78" height="30" />
-      <img src="/images/logoipsum-284.svg" alt="Brand 5" width="70" height="44" />
-      <img src="/images/logoipsum-215.svg" alt="Brand 6" width="132" height="40" />
+      <img src="/images/asus.svg" alt="Brand 1" width="70" height="30" />
+      <img src="/images/razer.svg" alt="Brand 2" width="70" height="30" />
+      <img src="/images/logitech.svg" alt="Brand 5" width="65" height="25" />
+      <img src="/images/alien.svg" alt="Brand 3" width="70" height="30" />
+      <img src="/images/msi.svg" alt="Brand 4" width="80" height="35" />
+      <img src="/images/nvidia.svg" alt="Brand 5" width="80" height="35" />
+      <img src="/images/lenovo.svg" alt="Brand 6" width="150" height="30" />
+      <img src="/images/predator.svg" alt="Brand 6" width="85" height="30" />
     </div>
 
     <section class="container my-16">
