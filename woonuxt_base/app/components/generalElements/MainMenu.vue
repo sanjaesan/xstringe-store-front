280844175fc0a6befd31f46f05adca4e4b71f46a
sanjaesan <sanusijubril.sj@gmail.com>
2025-03-04 18:44:43 +0100

    navbar link edited

--- woonuxt_base/app/components/generalElements/MainMenu.vue
+++ woonuxt_base/app/components/generalElements/MainMenu.vue
@@ -4,10 +4,10 @@ const { wishlistLink } = useAuth();
 
 <template>
   <nav>
-    <NuxtLink to="/custom">{{ $t('messages.general.custom') }}</NuxtLink>
-    <NuxtLink to="/Laptops">{{ $t('messages.general.laptop') }}</NuxtLink>
-    <NuxtLink to="/Desktops">{{ $t('messages.general.desktop') }}</NuxtLink>
-    <NuxtLink to="/Accessories">{{ $t('messages.general.accessories') }}</NuxtLink>
+    <NuxtLink to="/product-category/custom">{{ $t('messages.general.custom') }}</NuxtLink>
+    <NuxtLink to="/product-category/laptop">{{ $t('messages.general.laptop') }}</NuxtLink>
+    <NuxtLink to="/product-category/desktop">{{ $t('messages.general.desktop') }}</NuxtLink>
+    <NuxtLink to="/product-category/accessories">{{ $t('messages.general.accessories') }}</NuxtLink>
     <NuxtLink class="lg:hidden" :to="wishlistLink" :prefetch="false">Wishlist</NuxtLink>
     <NuxtLink class="lg:hidden" to="/my-account" :prefetch="false">My Account</NuxtLink>
   </nav>
