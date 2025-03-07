5ec475d7ae6612057a9083504aac74719727d83f
sanjaesan <sanusijubril.sj@gmail.com>
2025-03-07 18:28:18 +0100

    badge added to nav link and commented out

--- woonuxt_base/app/components/generalElements/MainMenu.vue
+++ woonuxt_base/app/components/generalElements/MainMenu.vue
@@ -4,7 +4,10 @@ const { wishlistLink } = useAuth();
 
 <template>
   <nav>
-    <NuxtLink class="hover:text-primary" to="/product-category/custom">{{ $t('messages.general.madeToOrder') }}</NuxtLink>
+    <NuxtLink class="hover:text-primary relative inline-flex items-center" to="/product-category/custom">
+      {{ $t('messages.general.madeToOrder') }}
+      <!-- <span class="absolute top-[-1.5em] right-[-3em] bg-red-500 text-white text-xs px-2 py-0.5 rounded-full">New!</span> -->
+    </NuxtLink>
     <NuxtLink class="hover:text-primary" to="/product-category/laptop">{{ $t('messages.general.laptop') }}</NuxtLink>
     <NuxtLink class="hover:text-primary" to="/product-category/desktop">{{ $t('messages.general.desktop') }}</NuxtLink>
     <NuxtLink class="hover:text-primary" to="/product-category/accessories">{{ $t('messages.general.accessories') }}</NuxtLink>
