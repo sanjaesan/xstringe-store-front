5b58d4abd2166105fe3a7b1f7938ed240e4e2962
sanjaesan <sanusijubril.sj@gmail.com>
2025-03-07 16:10:30 +0100

    nav link show primary color on hover

--- woonuxt_base/app/components/generalElements/MainMenu.vue
+++ woonuxt_base/app/components/generalElements/MainMenu.vue
@@ -4,10 +4,10 @@ const { wishlistLink } = useAuth();
 
 <template>
   <nav>
-    <NuxtLink to="/product-category/custom">{{ $t('messages.general.madeToOrder') }}</NuxtLink>
-    <NuxtLink to="/product-category/laptop">{{ $t('messages.general.laptop') }}</NuxtLink>
-    <NuxtLink to="/product-category/desktop">{{ $t('messages.general.desktop') }}</NuxtLink>
-    <NuxtLink to="/product-category/accessories">{{ $t('messages.general.accessories') }}</NuxtLink>
+    <NuxtLink class="hover:text-primary" to="/product-category/custom">{{ $t('messages.general.madeToOrder') }}</NuxtLink>
+    <NuxtLink class="hover:text-primary" to="/product-category/laptop">{{ $t('messages.general.laptop') }}</NuxtLink>
+    <NuxtLink class="hover:text-primary" to="/product-category/desktop">{{ $t('messages.general.desktop') }}</NuxtLink>
+    <NuxtLink class="hover:text-primary" to="/product-category/accessories">{{ $t('messages.general.accessories') }}</NuxtLink>
     <NuxtLink class="lg:hidden" :to="wishlistLink" :prefetch="false">Wishlist</NuxtLink>
     <NuxtLink class="lg:hidden" to="/my-account" :prefetch="false">My Account</NuxtLink>
   </nav>
