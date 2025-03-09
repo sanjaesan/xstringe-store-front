a1cb234321a59c9860b9fc90151d42ff362e3ea4
sanjaesan <sanusijubril.sj@gmail.com>
2025-03-09 23:58:57 +0100

    button color and hover edit

--- woonuxt_base/app/components/cartElements/CartTrigger.vue
+++ woonuxt_base/app/components/cartElements/CartTrigger.vue
@@ -4,7 +4,7 @@ const { toggleCart, cart } = useCart();
 
 <template>
   <div class="relative cursor-pointer inline-flex" title="Cart" @click="toggleCart">
-    <Icon name="ion:cart-outline" size="22" class="mr-1 md:mr-0" />
+    <Icon name="ion:cart-outline" size="22" class="bg-black hover:bg-primary mr-1 md:mr-0" />
     <ClientOnly>
       <Transition name="popIn" mode="out-in">
         <span
