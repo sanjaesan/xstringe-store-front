db39bf9f9ad25bf3878cde458d1df0fa9a284899
sanjaesan <sanusijubril.sj@gmail.com>
2025-03-09 23:36:20 +0100

    button color and hover edit

--- woonuxt_base/app/pages/checkout.vue
+++ woonuxt_base/app/pages/checkout.vue
@@ -164,7 +164,7 @@ useSeoMeta({
 
         <OrderSummary>
           <button
-            class="flex items-center justify-center w-full gap-3 p-3 mt-4 font-semibold text-center text-white rounded-lg shadow-md bg-primary hover:bg-primary-dark disabled:cursor-not-allowed disabled:bg-gray-400"
+            class="flex items-center justify-center w-full gap-3 p-3 mt-4 font-semibold text-center text-white rounded-lg shadow-md bg-black hover:bg-primary disabled:cursor-not-allowed disabled:bg-gray-400"
             :disabled="isCheckoutDisabled">
             {{ buttonText }}<LoadingIcon v-if="isProcessingOrder" color="#fff" size="18" />
           </button>
