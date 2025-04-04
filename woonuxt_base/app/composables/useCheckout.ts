cebb0dc1dd0ea2eb355a73ad870c522d646263c4
sanjaesan <sanusijubril.sj@gmail.com>
2025-04-04 11:33:55 +0100

    open paypal window func removed

--- woonuxt_base/app/composables/useCheckout.ts
+++ woonuxt_base/app/composables/useCheckout.ts
@@ -34,22 +34,6 @@ export function useCheckout() {
     }
   }
 
-  function openPayPalWindow(redirectUrl: string): Promise<boolean> {
-    return new Promise((resolve) => {
-      const width = 750;
-      const height = 750;
-      const left = window.innerWidth / 2 - width / 2;
-      const top = window.innerHeight / 2 - height / 2 + 80;
-      const payPalWindow = window.open(redirectUrl, '', `width=${width},height=${height},top=${top},left=${left}`);
-      const timer = setInterval(() => {
-        if (payPalWindow?.closed) {
-          clearInterval(timer);
-          resolve(true);
-        }
-      }, 500);
-    });
-  }
-
   const proccessCheckout = async (isPaid = false) => {
     const { customer, loginUser } = useAuth();
     const router = useRouter();
