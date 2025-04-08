e6b4519924be8ae46fc86e90188a3c5bb0552b20
Oluwaseyi Joshua <seyijosh44@gmail.com>
2025-04-08 17:55:47 +0100

    fixed badge z-index

--- woonuxt_base/app/components/productElements/SaleBadge.vue
+++ woonuxt_base/app/components/productElements/SaleBadge.vue
@@ -27,7 +27,7 @@ const textToDisplay = computed(() => {
 
 <style lang="postcss" scoped>
 .red-badge {
-  @apply rounded-md bg-red-400 text-xs text-white tracking-tight px-1.5 leading-6 z-10;
+  @apply rounded-md bg-red-400 text-xs text-white tracking-tight px-1.5 leading-6 z-[5];
   background: #000 linear-gradient(0deg, #f87171, #f87171);
 }
 </style>
