e72b1a4313662858638cd61776faf18c1e3ab6d1
sanjaesan <sanusijubril.sj@gmail.com>
2025-04-25 08:21:50 +0100

    spacing edit

--- woonuxt_base/app/components/filtering/Filters.vue
+++ woonuxt_base/app/components/filtering/Filters.vue
@@ -24,7 +24,7 @@ const attributesWithTerms = globalProductAttributes.map((attr) => ({ ...attr, te
 <template>
   <aside id="filters">
     <OrderByDropdown class="block w-full md:hidden" />
-    <div class="relative z-30 grid mb-12 space-y-8 divide-y">
+    <div class="relative z-30 grid mb-12 space-y-3 divide-y">
       <PriceFilter />
       <CategoryFilter v-if="!hideCategories" :terms="productCategoryTerms" />
       <div v-for="attribute in attributesWithTerms" :key="attribute.slug">
