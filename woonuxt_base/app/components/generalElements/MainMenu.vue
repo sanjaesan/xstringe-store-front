1ea66ceaa864ca81994085d5ef3f90cca07ca34b
sanjaesan <sanusijubril.sj@gmail.com>
2025-05-19 18:11:45 +0100

    badge label changed to new

--- woonuxt_base/app/components/generalElements/MainMenu.vue
+++ woonuxt_base/app/components/generalElements/MainMenu.vue
@@ -6,7 +6,7 @@ const { wishlistLink } = useAuth();
   <nav>
     <NuxtLink class="hover:text-primary relative inline-flex items-center mr-4" to="/product-category/made-to-order">
       {{ $t('messages.general.madeToOrder') }}
-      <span class="absolute top-[-1.3em] right-[-2.8em] bg-primary text-white text-xs px-2 py-0.5 rounded-full">New!</span>
+      <span class="absolute top-[-1.3em] right-[-2.8em] bg-primary text-white text-xs px-2 py-0.5 rounded-full">Hot!</span>
     </NuxtLink>
     <NuxtLink class="hover:text-primary mr-4" to="/product-category/laptop">{{ $t('messages.general.laptop') }}</NuxtLink>
     <NuxtLink class="hover:text-primary mr-4" to="/product-category/desktop">{{ $t('messages.general.desktop') }}</NuxtLink>
